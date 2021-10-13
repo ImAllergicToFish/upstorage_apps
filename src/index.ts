4157332f6ab6222efc2526c78d28e25da5e8073c
Iakimenko Grigorii <nneeleps@gmail.ru>
2021-10-13 22:25:12 +0300

    aws

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import updateApps from './updateApps';
 
 const PORT = 8080;
 
-api.listen(PORT, '3.16.21.96',() => {
+api.listen(PORT, '0.0.0.0',() => {
     updateApps()
     setInterval(updateApps, 1000*3600*24)
    console.log(`Listening on port ${PORT}`);
