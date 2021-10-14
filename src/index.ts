79abcbc0422d455aa1007fef72337c36d30d4739
g.yakimenko <nneeleps@gmail.com>
2021-10-14 13:35:23 +0300

    update interval edited

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const PORT = 3000;
 
 api.listen(PORT, '0.0.0.0',() => {
     updateApps()
-    setInterval(updateApps, 1000*3600*24)
+    setInterval(updateApps, 1000*60)
    console.log(`Listening on port ${PORT}`);
 });
  
