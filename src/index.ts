dd904f17e89cd611fff9f175df0622de83b7f903
g.yakimenko <nneeleps@gmail.com>
2022-04-01 13:40:58 +0300

    comment git clone/pull

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,8 @@ import pullAppsFromGit from './updateAppService/pullAppsFromGit';
 const PORT = 3001;
 
 api.listen(PORT, '0.0.0.0',() => {
-    cloneAppsFromGit()
-    setInterval(pullAppsFromGit, 1000*60)
+    // cloneAppsFromGit()
+    // setInterval(pullAppsFromGit, 1000*60)
    console.log(`Listening on port ${PORT}`);
 });
  
