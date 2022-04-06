dac328ba8034479eeec4e3babb99cb35688098b1
g.yakimenko <nneeleps@gmail.com>
2022-04-06 17:50:56 +0300

    run script add block

--- src/api/runScript/run.ts
+++ src/api/runScript/run.ts
@@ -1,14 +1,18 @@
 import { execSync } from "child_process";
+import BlockStatus from "../../blockStatus";
 
 export default (req, res, next) => {
     try {
-        
+        BlockStatus.set(false);
+
         const script: string = '../' + req.params.script;
         execSync(script,  {stdio: 'inherit', cwd: '../'});
-        res.status(200).send('true')
+        res.status(200).send('true');
+
+        BlockStatus.set(true);
 
     }
     catch(e) {
-        res.status(404).send("SCRIPT NOT FOUND")
+        res.status(404).send("SCRIPT NOT FOUND");
     }
 }
